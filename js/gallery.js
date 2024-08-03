eea4b33644258906d0c6c2d1c3665821be9041c5
yuurigit <yuuri.szk@gmail.com>
2024-08-02 21:34:44 -0400

    galleryfinish

--- js/gallery.js
+++ js/gallery.js
@@ -1,25 +1,27 @@
 let imageCards = document.querySelectorAll(".gallery");
 console.log(imageCards);
 
-
 imageCards.forEach( thisCard => {
+    // let photo = thisCard.querySelector(".photo");
     thisCard.addEventListener("mouseenter", function(){
+        // console.log(photo);
         // scaling effect on hovers over
-        thisCard.classList.add("scaleUp");
+        thisCard.firstElementChild.classList.add("scaleUp");
         // fade-in effect for the project titles on hovers over 
-        // thisCard.firstElementChild.nextElementSibling.classList.add("fadeIn");
-        thisCard.firstElementChild.classList.add("fadeInText");
+        thisCard.firstElementChild.nextElementSibling.classList.add("fadeIn");
+        thisCard.firstElementChild.nextElementSibling.nextElementSibling.classList.add("fadeInText");
     })
 })
+
 
 imageCards.forEach( thisCard => {
     thisCard.addEventListener("mouseleave", function(){
         
         setTimeout(function(){
-            thisCard.classList.remove("scaleUp");
+            thisCard.firstElementChild.classList.remove("scaleUp");
         }, 500)
 
-        // thisCard.firstElementChild.nextElementSibling.classList.remove("fadeIn");
-        thisCard.firstElementChild.classList.remove("fadeInText");
+        thisCard.firstElementChild.nextElementSibling.classList.remove("fadeIn");
+        thisCard.firstElementChild.nextElementSibling.nextElementSibling.classList.remove("fadeInText");
     })
 })
